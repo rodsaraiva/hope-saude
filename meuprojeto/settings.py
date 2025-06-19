00ffea02e052ba7c32abd02f619d9eea2f51404e
Rodrigo Saraiva <rassaraiva@gmail.com>
2025-06-19 20:30:31 -0300

    Implementa DB postgres

--- meuprojeto/settings.py
+++ meuprojeto/settings.py
@@ -110,10 +110,21 @@ WSGI_APPLICATION = 'meuprojeto.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT', '5432'), # '5432' é o padrão, útil como fallback
     }
 }
 
